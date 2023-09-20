3f0acae47b0a7e2747b99b17a26e2dd24dfe5aba
Natalia <nataliazanella@yahoo.com.br>
2023-09-20 14:07:53 -0300

    atualizacao-icones

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -42,7 +42,7 @@
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
-                        <i class="nav-icon fa-solid fa-table fa-lg"></i>
+                        <i class="nav-icon fa-solid fa-table"></i>
                         <p>
                             Adminstrador
                             <i class="right fas fa-angle-left"></i>
